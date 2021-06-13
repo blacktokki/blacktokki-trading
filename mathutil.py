d8cdae62ce51dc835922301056d1e4062fc8778e
blacktokki <39031723+blacktokki@users.noreply.github.com>
2021-06-13 12:09:26 +0900

    확률분포 정리2

--- mathutil.py
+++ mathutil.py
@@ -3,4 +3,4 @@ def cdf(z):  # p-value
     return  (1.0 + math.erf(z / math.sqrt(2.0))) / 2.0
 
 def laplace_cdf(z):
-    return 0.5 * (1 + math.copysign(1, z) * (1 - math.exp(math.copysign(z, -1) * math.sqrt(0.5))))
+    return 0.5 * (1 + math.copysign(1, z) * (1 - math.exp(math.copysign(z, -1) * math.sqrt(2))))
